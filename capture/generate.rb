714b5b0df9f0ddb3e484e39f4a1840ebdf02426c
Saku Ytti <saku@ytti.fi>
2015-02-28 13:32:04 +0200

    add tests
    unfortunately cannot add data to test against, would need to generate
    some clean room data

---
+++ capture/generate.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+
+require 'packet_via_dmem'
+
+# takes 1 or more argument pointing to filename as 'input-something.txt'
+# this input should be data gathered from juniper
+# writes to output received-something.txt, sent-something.txt,
+#  original-something.txt and header-something.txt
+# This is used to generate files for testing
+
+class PacketViaDMEM
+  class CreateTestFiles
+    def initialize
+      @dmem = PacketViaDMEM.new
+    end
+    def run file
+      name, ext = file.split '.'
+      return unless ext == 'txt'
+      direction, name  = name.split '-'
+      return unless direction == 'input'
+      headers, received, sent, originals = [], [], [], []
+      @dmem.parse(File.read(file)).each do |packet|
+        case packet.type
+        when :received then received << packet.packet.join
+        when :sent     then sent     << packet.packet.join
+        else raise StandardError, "invalid type #{type}"
+        end
+        headers   << packet.header.join
+        originals << packet.original.join
+      end
+      File.write('header-'   + name + '.txt', headers.join("\n"))
+      File.write('sent-'     + name + '.txt', sent.join("\n"))
+      File.write('received-' + name + '.txt', received.join("\n"))
+      File.write('original-' + name + '.txt', originals.join("\n"))
+    end
+  end
+end
+
+runner = PacketViaDMEM::CreateTestFiles.new
+ARGV.each do |arg|
+  runner.run arg
+end
---
+++ spec/packet_via_dmem_spec.rb
@@ -0,0 +1,85 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/packet_via_dmem'
+
+CAPTURES = File.expand_path File.join(File.dirname(__FILE__), '..', 'capture')
+
+describe PacketViaDMEM do
+
+  before do
+    @dmem = PacketViaDMEM.new
+    @input    = {}
+    @sent     = {}
+    @received = {}
+    @original = {}
+    @header   = {}
+    Dir.glob(File.join(CAPTURES, '*.txt')) do |entry|
+      _directory, file = File.split entry
+      file, _ext = file.split '.'
+      type, name = file.split '-'
+      next unless name
+      case type
+      when 'input'    then @input[name]    = File.read entry
+      when 'received' then @received[name] = File.read(entry).split
+      when 'sent'     then @sent[name]     = File.read(entry).split
+      when 'original' then @original[name] = File.read(entry).split
+      when 'header'   then @header[name]   = File.read(entry).split
+      end
+    end
+  end
+
+  describe '#parse' do
+    before do
+      @parsed = {}
+      @input.each do |name, str|
+        @parsed[name] = @dmem.parse str
+      end
+    end
+
+    it 'returns as many times as called' do
+      @parsed.size.must_equal @input.size
+    end
+
+    it 'produces correct headers' do
+      @parsed.each do |name, packets|
+        packets.each_with_index do |packet, index|
+          packet.header.join.must_equal @header[name][index], "header #{name} at line #{index+1}"
+        end
+        packets.size.must_equal @header[name].size, "header #{name} has incorrect amount of packets"
+      end
+    end
+
+    it 'produces correct received frames' do
+      @parsed.each do |name, packets|
+        index = -1
+        packets.each do |packet|
+          next unless packet.type == :received
+          packet.packet.join.must_equal @received[name][index+=1], "received #{name} at line #{index+1}"
+        end
+        @received[name].size.must_equal index+1, "received #{name} has incorrect amount of packets"
+      end
+    end
+
+    it 'produces correct sent frames' do
+      @parsed.each do |name, packets|
+        index = -1
+        packets.each do |packet|
+          next unless packet.type == :sent
+          packet.packet.join.must_equal @sent[name][index+=1], "sent #{name} at line #{index+1}"
+        end
+        @sent[name].size.must_equal index+1, "sent #{name} has incorrect amount of packets"
+      end
+    end
+
+    it 'produces correct original frames' do
+      @parsed.each do |name, packets|
+        packets.each_with_index do |packet, index|
+          packet.original.join.must_equal @original[name][index], "original #{name} at line #{index+1}"
+        end
+        packets.size.must_equal @header[name].size, "header #{name} has incorrect amount of packets"
+      end
+    end
+
+  end
+
+end
